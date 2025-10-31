c8d3a9fced7e5a69b2d508fa2597cbdb1130176e
Rohan2427 <rohansonwane8@gmail.com>
2025-10-31 11:58:58 +0530

    JS file added

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://admin:yourpassword@localhost:27017/todo-app?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Todo Schema
+const Todo = mongoose.model('Todo', { text: String, completed: Boolean });
+
+// API Endpoints
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+  const todo = new Todo(req.body);
+  await todo.save();
+  res.json(todo);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
